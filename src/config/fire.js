0ecc8782862b81c6084890738bd4f4160154d66f
Dan Donohue <dan.donohue@ef.com>
2018-11-29 14:48:36 -0500

    Oops

--- src/config/fire.js
+++ src/config/fire.js
@@ -1,14 +1,13 @@
 import firebase from "firebase";
 
 var config = {
-  apiKey: process.env.FBAPIKEY,
-  authDomain: "little-fighter-dude.firebaseapp.com",
-  databaseURL: "https://little-fighter-dude.firebaseio.com",
-  projectId: "little-fighter-dude",
-  storageBucket: "little-fighter-dude.appspot.com",
-  messagingSenderId: process.env.MSGSENDERID
+  apiKey: "process.env.FBAPIKEY",
+  authDomain: "bjj-journal-c7c89.firebaseapp.com",
+  databaseURL: "https://bjj-journal-c7c89.firebaseio.com",
+  projectId: "bjj-journal-c7c89",
+  storageBucket: "",
+  messagingSenderId: "process.env.MSGSENDERID"
 };
-
 const fire = firebase.initializeApp(config);
 
 export default fire;
