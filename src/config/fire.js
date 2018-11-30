da1416e0150033d65bd6a88d18e41cec97dbf9a7
Dan Donohue <dan.donohue@ef.com>
2018-11-30 13:34:52 -0500

    Update4s for heroku

--- src/config/fire.js
+++ src/config/fire.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 var config = {
-  apiKey: "process.env.FBAPIKEY",
+  apiKey: process.env.REACT_APP_FBAPIKEY,
   authDomain: "bjj-journal-c7c89.firebaseapp.com",
   databaseURL: "https://bjj-journal-c7c89.firebaseio.com",
   projectId: "bjj-journal-c7c89",
   storageBucket: "",
-  messagingSenderId: "process.env.MSGSENDERID"
+  messagingSenderId: process.env.REACT_APP_MSGSENDERID
 };
 const fire = firebase.initializeApp(config);
 
